6e63ca6339d4d231b6b37138d5d1d8885a09370e
afortes <pekis.fs@gmail.com>
2014-04-16 19:15:52 +0200

    ListarUsuarios

---
+++ views/listarUsuarios.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Listar Usuarios">
+    <meta name="author" content="Alex Fortes">
+
+    <title>TuParty.com</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+
+    <!-- Add custom CSS here -->
+    <link href="../css/shop-item.css" rel="stylesheet">
+   
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#home">TuParty.com</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="#about">About</a>
+                    </li>
+                    <li><a href="#services">Services</a>
+                    </li>
+                    <li><a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-lg-3">
+                <p class="lead">Bienvenido</p>
+                <div class="list-group">
+                    <a href="#" class="list-group-item active">Navegación 1</a>
+                    <a href="#" class="list-group-item">Navegación 2</a>
+                    <a href="#" class="list-group-item">Navegación 3</a>
+                </div>
+            </div>
+
+            <div class="col-lg-9">
+
+                <div class="thumbnail">
+                    
+                    <div class="caption-full">
+                        
+                        <h4>Lista de usuarios de la aplicación</h4>                       </h4>
+                         <p>
+                      <!-- Muestra la lista de usuarios -->
+
+                               
+                                  
+                                  
+                                  
+                    </div>
+                   
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <div class="container">
+
+        <hr>
+
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Company 2013 - Template by <a href="http://maxoffsky.com/">Maks</a>
+                    </p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- JavaScript -->
+    <script src="js/jquery-1.10.2.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+</body>
+
+</html>
